df16f0ed50b2b4da72acd5837819eccbf7fc9436
nikitakalnov <jdmcorolla21@gmail.com>
2020-12-26 13:18:30 +0400

    Function to create int expr

--- GoCompiler/create_tree_nodes.c
+++ GoCompiler/create_tree_nodes.c
@@ -1,3 +1,10 @@
 #include "create_tree_nodes.h"
 #include "malloc.h"
 
+struct expr_struct* create_int_expr(int value) {
+    struct expr_struct* result = (struct expr_struct*)malloc(sizeof(struct expr_struct));
+    result->type = integer;
+    result->int_value = value;
+    return result;
+}
+
