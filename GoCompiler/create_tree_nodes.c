0a37c150a56af77766f59fbb8fcc67b76a51204d
nikitakalnov <jdmcorolla21@gmail.com>
2020-12-26 14:01:34 +0400

    Implemented functions for expr_stmt creating

--- GoCompiler/create_tree_nodes.c
+++ GoCompiler/create_tree_nodes.c
@@ -8,3 +8,18 @@ struct expr_struct* create_int_expr(int value) {
     return result;
 }
 
+struct expr_struct* create_string_expr(char* value) {
+    struct expr_struct* result = (struct expr_struct*)malloc(sizeof(struct expr_struct));
+    result->type = str;
+    result->str_value = value;
+    return result;
+}
+
+struct expr_struct* create_operation_expr(enum expr_type type, struct expr_struct* left, struct expr_struct* right) {
+    struct expr_struct* result = (struct expr_struct*)malloc(sizeof(struct expr_struct));
+    result->type = type;
+    result->left = left;
+    result->right = right;
+
+    return result;
+}
