96c56ba0fd39519680364c38850964d0d617060f
nikitakalnov <jdmcorolla21@gmail.com>
2020-12-26 14:19:08 +0400

    Changed return type to stmt_struct for if and for stmt creating functions

--- GoCompiler/create_tree_nodes.h
+++ GoCompiler/create_tree_nodes.h
@@ -3,12 +3,12 @@
 struct expr_struct* create_int_expr(int value);
 struct expr_struct* create_string_expr(char* value);
 struct expr_struct* create_operation_expr(enum expr_type type, struct expr_struct* left, struct expr_struct* right);
-struct for_stmt_struct* create_empty_for_stmt(struct stmt_struct* block);
-struct for_stmt_struct* create_for_with_condition(struct expr_struct* condition, struct stmt_struct* block);
-struct for_stmt_struct* create_for_clause_stmt(
+struct stmt_struct* create_empty_for_stmt(struct stmt_struct* block);
+struct stmt_struct* create_for_with_condition(struct expr_struct* condition, struct stmt_struct* block);
+struct stmt_struct* create_for_clause_stmt(
 	struct stmt_struct* init_stmt, struct stmt_struct* post_stmt, struct expr_struct* condition, struct stmt_struct* block
 );
-struct if_stmt_struct* create_if_stmt(
+struct stmt_struct* create_if_stmt(
 	struct if_stmt_part_struct* if_stmt_part,
 	struct else_if_stmt_list_struct* else_if_stmts,
 	struct stmt_struct* else_block
