0f132aa6556b7a67795d4cb9a39df5cc013391b7
Данила Кальнов <danyakalnov@gmail.com>
2020-12-28 18:28:15 +0300

    Implement create_id_expr function

--- GoCompiler/create_tree_nodes.c
+++ GoCompiler/create_tree_nodes.c
@@ -22,6 +22,14 @@ struct expr_struct* create_boolean_expr(int value) {
     return result;
 }
 
+struct expr_struct* create_id_expr(char* id) {
+    struct expr_struct* result = (struct expr_struct*)malloc(sizeof(struct expr_struct));
+    result->type = id;
+    result->str_value = id;
+
+    return result;
+}
+
 struct expr_struct* create_operation_expr(enum expr_type type, struct expr_struct* left, struct expr_struct* right) {
     struct expr_struct* result = (struct expr_struct*)malloc(sizeof(struct expr_struct));
     result->type = type;
