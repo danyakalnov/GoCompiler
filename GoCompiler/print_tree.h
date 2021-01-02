390235e38d21737cfc2b9909db726dd76214ef77
Данила Кальнов <danyakalnov@gmail.com>
2021-01-02 13:36:12 +0300

    Refactoring: print_block receive stmt_block_struct as the first argument

--- GoCompiler/print_tree.h
+++ GoCompiler/print_tree.h
@@ -17,7 +17,7 @@ void print_declaration_spec(struct decl_spec_struct* spec, FILE* output_file);
 void print_declaration_spec_list(struct decl_spec_list_struct* spec_list, FILE* output_file);
 void print_stmt(struct stmt_struct* stmt, FILE* output_file);
 void print_expr(struct expr_struct* expr, FILE* output_file);
-void print_block(struct stmt_struct* block, FILE* output_file);
+void print_block(struct stmt_block_struct* block, FILE* output_file);
 void print_stmt_list(struct stmt_list_struct* list, void* parent, FILE* output_file);
 void print_if(struct if_stmt_struct* if_stmt, FILE* output_file);
 void print_for(struct for_stmt_struct* for_stmt, FILE* output_file);
